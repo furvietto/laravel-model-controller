1311ea215628b7a4890be8aacdb7ee7fb883313c
furvietto <fulvio-94@hotmail.it>
2022-02-17 14:14:05 +0100

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class PageController extends Controller
+{
+    public function index() 
+    {
+       $movie =  Movie::all();
+
+       dd($movie);
+        return view('home');
+    }
+}
